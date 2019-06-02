1c034a036dfce19b87d249104ab0ec0671267ebd
leimark <51204928+leimark@users.noreply.github.com>
2019-06-02 12:55:08 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCalculateFibonacci(t *testing.T) {
 
-	array := calculateFibonacci(1)
+	array := CalculateFibonacci(1)
 
 	if array == nil || len(array) != 1 || array[0] != 0{
 		t.Fatal("calculateFibonacci(1) result is wrong. Should be [0], now is: ")
@@ -16,7 +16,7 @@ func TestCalculateFibonacci(t *testing.T) {
 
 	t.Log("calculateFibonacci(1) result is correct!")
 
-	array = calculateFibonacci(2)
+	array = CalculateFibonacci(2)
 
 	if array == nil || len(array) != 2 || array[0] != 0 || array[1] != 1{
 
@@ -29,7 +29,7 @@ func TestCalculateFibonacci(t *testing.T) {
 
 	var correctResult =[...]uint64 {0, 1, 1, 2, 3}
 
-	array = calculateFibonacci(5)
+	array = CalculateFibonacci(5)
 
 	if array == nil || len(array) != 5 || reflect.DeepEqual(correctResult, array)!=true {
 
